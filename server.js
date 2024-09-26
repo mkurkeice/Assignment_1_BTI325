e9406dfe22c7f3f8e8e264d0bfe94778b12a3055
Manualla Kurkeice <mkurkeice@myseneca.ca>
2024-09-26 17:25:54 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+/*********************************************************************************
+*  BTI325 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: _Manualla_Kurkeice__ Student ID: __163009228___ Date: ___9/26/2024____
+*
+*  Online (Vercel) URL: _______________________________________________________
+*
+********************************************************************************/ 
+
+
+/* app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+require('pg'); // explicitly require the "pg" module
+const Sequelize = require('sequelize'); */
+
+//im using the arrow function - ES6 function syntax
+const helloWorld = () => {        
+    console.log("Manualla Kurkeice - 163009228")
+}
+helloWorld();
